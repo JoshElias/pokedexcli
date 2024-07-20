4dfbb8ae60ac5f935edfb118a2f069c178db2f24
Josh Elias <josh@daemontech.tools>
2024-07-20 11:29:19 -0400

    refactor(api): seperate command logic

---
+++ commands.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Commands map[string]cliCommand
+
+func init() {
+	RegisterCommand("help", "Displays a help message", commandHelp)
+	RegisterCommand("exit", "Exit the Pokedex", commandExit)
+}
+
+func RegisterCommand(name string, description string, commandFunc func() error) {
+	if _, ok := Commands[name]; ok {
+		fmt.Println("overriding existing command: ", name)
+	}
+	Commands[name] = cliCommand{
+		name:        name,
+		description: description,
+		callback:    commandFunc,
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exiting Pokedex...")
+	return nil
+}
--- main.go
+++ main.go
@@ -2,30 +2,7 @@ package main
 
 import (
 	"fmt"
-	"internal/pokeapi"
 )
-
-var commands map[string]cliCommand
-
-func init() {
-	commands = map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"test": {
-			name:        "test",
-			description: "Test whatever I'm currently working on",
-			callback:    commandTest,
-		},
-	}
-}
 
 func main() {
 	var input string
@@ -43,26 +20,4 @@ func main() {
 		}
 
 	}
-}
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
-	return nil
-}
-
-func commandExit() error {
-	fmt.Println("Exiting Pokedex...")
-	return nil
-}
-
-func commandTest() error {
-	fmt.Println("Calling test")
-	pokeapi.GetLocationAreas()
-	return nil
 }
