4c1b9bc2b8b67dac1fa75ee67ceb16cb8f2c2549
Josh Elias <josh@daemontech.tools>
2024-07-20 16:14:40 -0400

    feat(api): add structs for LocationArea

--- pokeapi.go
+++ pokeapi.go
@@ -2,13 +2,87 @@ package main
 
 import (
 	"fmt"
-	// "github.com/mtslzr/pokeapi-go"
 )
 
 const POKEAPI_URL = "https://pokeapi.co/api/v2/location"
 
+type LocationArea struct {
+	ID                   int                    `json:"id"`
+	Name                 string                 `json:"name"`
+	GameIndex            int                    `json:"game_index"`
+	EncounterMethodRates []EncounterMethodRates `json:"encounter_method_rates"`
+	Location             Location               `json:"location"`
+	Names                []Names                `json:"names"`
+	PokemonEncounters    []PokemonEncounters    `json:"pokemon_encounters"`
+}
+
+type EncounterMethod struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Version struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type EncounterMethodVersionDetails struct {
+	Rate    int     `json:"rate"`
+	Version Version `json:"version"`
+}
+
+type EncounterMethodRates struct {
+	EncounterMethod EncounterMethod                 `json:"encounter_method"`
+	VersionDetails  []EncounterMethodVersionDetails `json:"version_details"`
+}
+
+type Location struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Language struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Names struct {
+	Name     string   `json:"name"`
+	Language Language `json:"language"`
+}
+
+type Pokemon struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Method struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type EncounterDetails struct {
+	MinLevel        int    `json:"min_level"`
+	MaxLevel        int    `json:"max_level"`
+	ConditionValues []any  `json:"condition_values"`
+	Chance          int    `json:"chance"`
+	Method          Method `json:"method"`
+}
+
+type EncounterVersionDetails struct {
+	Version          Version            `json:"version"`
+	MaxChance        int                `json:"max_chance"`
+	EncounterDetails []EncounterDetails `json:"encounter_details"`
+}
+
+type PokemonEncounters struct {
+	Pokemon        Pokemon                   `json:"pokemon"`
+	VersionDetails []EncounterVersionDetails `json:"version_details"`
+}
+
 func GetLocationAreas() {
 	fmt.Println("Pokeapi URL: ", POKEAPI_URL)
+
 	// fmt.Println("Getting Location Areas")
 	// a, b := pokeapi.Resource("location-area")
 	// fmt.Println(a)
