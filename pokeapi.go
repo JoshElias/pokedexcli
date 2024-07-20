3aaf80c8d5108a05f19be804715174ad20985715
Josh Elias <josh@daemontech.tools>
2024-07-20 11:27:49 -0400

    refactor(api): add local file for pokeapi

---
+++ pokeapi.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/mtslzr/pokeapi-go"
+)
+
+const POKEAPI_URL = "https://pokeapi.co/api/v2/location"
+
+func GetLocationAreas() {
+	fmt.Println("Pokeapi URL: ", POKEAPI_URL)
+	// fmt.Println("Getting Location Areas")
+	// a, b := pokeapi.Resource("location-area")
+	// fmt.Println(a)
+	// fmt.Println(b)
+}
---
+++ pokeapi_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetLocationAreas(t *testing.T) {
+	GetLocationAreas()
+}
