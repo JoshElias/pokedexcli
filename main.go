11344affe622e50b861d5f41a7c6606e2687ac53
Josh Elias <josh@daemontech.tools>
2024-07-19 21:17:42 -0400

    add repl loop with commands

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func main() {
+	var input string
+	for {
+		fmt.Print("Pokedex > ")
+		fmt.Scanln(&input)
+		if command, ok := commands[input]; !ok {
+			continue
+		} else {
+			command.callback()
+		}
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Exiting Pokedex...")
+	return nil
+}
