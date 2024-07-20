3e79f719cae501c33e5e4873e60d03149260c2b5
Josh Elias <josh@daemontech.tools>
2024-07-20 16:13:53 -0400

    feat(api): return []byte from GetRequest()

---
+++ request.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetRequest(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		err := fmt.Errorf("Response failed with status code: %d and body: %s", res.StatusCode, body)
+		log.Fatal(err)
+		return nil, err
+	}
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	fmt.Println("Received body: ", body)
+	return body, nil
+}
