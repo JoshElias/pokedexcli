4e069cfb9d137db68ead86bfad81c2597a11b2f5
Josh Elias <josh@daemontech.tools>
2024-07-27 22:20:37 -0400

    test(api): add get pokemon test

--- internal/pokeapi/pokeapi_test.go
+++ internal/pokeapi/pokeapi_test.go
@@ -1,6 +1,7 @@
 package pokeapi
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,5 +15,14 @@ func TestExplore(t *testing.T) {
 	const location = "canalave-city-area"
 	if err := Explore(location); err != nil {
 		t.Fatal("error exploring location")
+	}
+}
+
+func TestGetPokemon(t *testing.T) {
+	const pokemon = "pikachu"
+	if pokemon, err := GetPokemon(pokemon); err != nil {
+		t.Fatal("error exploring location")
+	} else {
+		fmt.Println(pokemon.Name)
 	}
 }
